8e8b63bea9c9194fbe24f7b507ac606d26242f85
worldOfRavi <rajendra20023@gmail.com>
2024-06-22 12:04:57 -0400

    added delpoyed url in frontend socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -12,7 +12,7 @@ export const SocketContextProvider = ({children})=>{
     const {authUser} = useAuthContext();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chat-app-fy98.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
